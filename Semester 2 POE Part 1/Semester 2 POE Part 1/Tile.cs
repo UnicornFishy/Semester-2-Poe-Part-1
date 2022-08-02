b200a81a364ba1389894683abd1c2507251d0544
Maxime <maximedeclerck104@gmail.com>
2022-08-02 10:39:13 +0200

    First Commit

---
+++ Semester 2 POE Part 1/Semester 2 POE Part 1/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public abstract class Tile
+{
+	protected int X;
+	protected int Y;
+
+	public int x { get { return X; } set { X = value; } };
+	public int y { get { return Y; } set { Y = value; } };
+
+	public enum tileType { Hero, Enemy, Gold, weapon };
+
+	public Tile( X, Y)
+	{
+
+	}
+}
