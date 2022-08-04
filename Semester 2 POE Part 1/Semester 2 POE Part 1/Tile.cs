153857c10f94afaaae8882f74ae6bb76dca065dd
Maxime <maximedeclerck104@gmail.com>
2022-08-04 15:12:30 +0200

    Tile Constructor finished

--- Semester 2 POE Part 1/Semester 2 POE Part 1/Tile.cs
+++ Semester 2 POE Part 1/Semester 2 POE Part 1/Tile.cs
@@ -7,16 +7,21 @@ using System.Threading.Tasks;
 
 public abstract class Tile
 {
-	protected int X;
-	protected int Y;
+	protected int x;
+	protected int y;
+	protected tileType type;
 
-	public int x { get { return X; } set { X = value; } };
-	public int y { get { return Y; } set { Y = value; } };
+	public int X { get { return x; } set { x = value; } };
+	public int Y { get { return y; } set { y = value; } };
 
 	public enum tileType { Hero, Enemy, Gold, weapon };
 
-	public Tile( X, Y)
+	public Tile(int X,int Y, tileType type)
 	{
-
+		this.x = X;
+		this.y = y;
+		this.type = type;
 	}
+
+	//Tile tile = new Tile(5, 7, tileType.Hero);
 }
