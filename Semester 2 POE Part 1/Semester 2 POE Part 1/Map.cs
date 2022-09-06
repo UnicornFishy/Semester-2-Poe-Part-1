ed98b0e4926fab10e7a3a62f36c02a3f083e764e
ALBooysen78 <albooysen78@gmail.com>
2022-09-06 09:49:52 +0200

    Added map borders

--- Semester 2 POE Part 1/Semester 2 POE Part 1/Map.cs
+++ Semester 2 POE Part 1/Semester 2 POE Part 1/Map.cs
@@ -21,7 +21,18 @@ namespace Semester_2_POE_Part_1
             this.mapWidth = random.Next(minWidth,maxWidth);
             map = new Tile[mapHeight,mapWidth];
             enemies = new Enemy[enemyNumber];
-            
+
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    if (i == 0 || i == mapHeight - 1 || j == 0 || j == mapWidth - 1)
+                    {
+                        map[i, j] = new Obstacle(i, j);
+                    }
+                    
+                }
+            }
         }
 
         public void UpdateVision()
@@ -29,6 +40,17 @@ namespace Semester_2_POE_Part_1
 
         }
 
+        private Tile Create(Tile.tileType type)
+        {
+            bool validPosition = false;
+            do
+            {
+                int X = random.Next(mapHeight);
+                int Y = random.Next(mapWidth);
 
+            } while (validPosition == false);
+
+            return null;
+        }
     }
 }
