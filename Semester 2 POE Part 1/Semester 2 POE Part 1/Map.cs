f4dcd7499da44772604ca66d850c44a3a8e8f172
ALBooysen78 <albooysen78@gmail.com>
2022-09-06 08:12:13 +0200

    Created Map class variables

--- Semester 2 POE Part 1/Semester 2 POE Part 1/Map.cs
+++ Semester 2 POE Part 1/Semester 2 POE Part 1/Map.cs
@@ -8,5 +8,11 @@ namespace Semester_2_POE_Part_1
 {
     internal class Map
     {
+        private Tile[,] map;
+        private int mapHeight;
+        private int mapWidth;
+        private Enemy[] enemies;
+        private Hero hero;
+        private Random random;
     }
 }
