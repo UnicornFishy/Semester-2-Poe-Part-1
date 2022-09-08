db982d9a89feb6541ada4fb8bf37d21a3f04bbde
ALBooysen78 <albooysen78@gmail.com>
2022-09-08 13:26:40 +0200

    seperated SwampCreature to it's own class file

---
+++ Semester 2 POE Part 1/Semester 2 POE Part 1/SwampCreature.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semester_2_POE_Part_1
+{
+    class SwampCreature : Enemy
+    {
+        public SwampCreature(int X, int Y) : base(X, Y, 10, 10, 1, "SC")
+        {
+
+        }
+        public override movement ReturnMove(movement enemyMovementSwampCreature)
+        {
+            Random rnd = new Random();
+            int randomEnemyMovementDirection = rnd.Next(5);
+
+            switch (randomEnemyMovementDirection)
+            {
+                case 0:
+                    if ((this.VISION[0] is Obstacle) || (this.VISION[0] is Hero))            // !Hero and !Obstacle do not work
+                    {
+                        //how do I make it loop?
+                        break;
+                    }
+                    else { enemyMovementSwampCreature = movement.up; }
+                    break;
+                case 1:
+                    if ((this.VISION[1] is Obstacle) || (this.VISION[1] is Hero))            // !Hero and !Obstacle do not work
+                    {
+                        break;
+                    }
+                    else { enemyMovementSwampCreature = movement.down; }
+
+                    break;
+                case 2:
+                    if ((this.VISION[2] is Obstacle) || (this.VISION[2] is Hero))            // !Hero and !Obstacle do not work
+                    {
+                        break;
+                    }
+                    else { enemyMovementSwampCreature = movement.left; }
+
+                    break;
+                case 3:
+                    if ((this.VISION[3] is Obstacle) || (this.VISION[3] is Hero))            // !Hero and !Obstacle do not work
+                    {
+                        break;
+                    }
+                    else { enemyMovementSwampCreature = movement.right; }
+
+                    break;
+                case 4:
+                    if ((this.VISION[4] is Obstacle) || (this.VISION[4] is Hero))            // !Hero and !Obstacle do not work
+                    {
+                        break;
+                    }
+                    else { enemyMovementSwampCreature = movement.NoMovement; }
+
+                    break;
+            }
+            return enemyMovementSwampCreature;
+        }
+    }
+}
