71c122fb5ce3e865eed94b7437f99f0144d3f070
ALBooysen78 <albooysen78@gmail.com>
2022-09-10 16:52:05 +0200

    Fixed map values

--- Semester 2 POE Part 1/Semester 2 POE Part 1/Map.cs
+++ Semester 2 POE Part 1/Semester 2 POE Part 1/Map.cs
@@ -57,13 +57,13 @@ namespace Semester_2_POE_Part_1
            
 
             //up
-            character.VISION[0] = map[character.X, character.Y + 1];
+            character.VISION[0] = map[character.X - 1, character.Y];
             //down
-            character.VISION[1] = map[character.X, character.Y - 1];
+            character.VISION[1] = map[character.X+ 1, character.Y];
             //left
-            character.VISION[2] = map[character.X - 1, character.Y];
+            character.VISION[2] = map[character.X, character.Y-1];
             //right
-            character.VISION[3] = map[character.X + 1, character.Y];
+            character.VISION[3] = map[character.X + 1, character.Y+1];
 
 
         }
